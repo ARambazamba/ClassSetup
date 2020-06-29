198b18566cfdfbdf08c464d5b991035e8dc23de4
Alexander Pajer <alexander.pajer@integrations.at>
2020-06-29 09:55:52 +0200

    chocolatery

---
+++ chocolatery/setup-machine.ps1
@@ -0,0 +1,10 @@
+
+choco install chrome-remote-desktop-chrome -y
+choco install microsoft-edge -y
+choco install vscode -y
+choco install microsoft-windows-terminal -y
+choco install dotnetcore-sdk -y
+choco install nvm -y
+nvm install 10.21.0
+nvm install 12.17.0
+choco install azure-cli -y
