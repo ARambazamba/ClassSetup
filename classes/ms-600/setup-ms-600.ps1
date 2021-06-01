0f2963da14a4c4e68689be0cc991ff892e420653
Alexander Pajer <alexander.pajer@integrations.at>
2021-06-01 11:47:34 +0200

    Update setup-ms-600.ps1

--- classes/ms-600/setup-ms-600.ps1
+++ classes/ms-600/setup-ms-600.ps1
@@ -4,17 +4,14 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install Software
-choco install microsoft-edge -y
 choco install googlechrome -y
 choco install vscode -y
 choco install dotnetcore-sdk -y
 choco install dotnet-5.0-sdk -y
 choco install git -y
-choco install gitextensions -y
 choco install nodejs-lts --version=12.18.4 -y
 choco install azure-cli -y
 choco install azure-functions-core-tools-3 --params="'/x64:true'" -y
-choco install azurepowershell -y
 choco install postman -y
 choco install ngrok -y
 
@@ -29,14 +26,12 @@ code --install-extension ms-vscode.powershell
 code --install-extension ms-vscode.azurecli
 code --install-extension ms-vscode.azure-account
 code --install-extension ms-azuretools.vscode-azureappservice
-code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-azuretools.vscode-azurefunctions
 code --install-extension GitHub.vscode-pull-request-github
 code --install-extension redhat.vscode-yaml
 code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension mdickin.markdown-shortcuts
 code --install-extension mhutchie.git-graph 
-code --install-extension ms-azure-devops.azure-pipelines
 code --install-extension xabikos.JavaScriptSnippets			
 code --install-extension eliostruyf.spfx-snippets
 code --install-extension TeamsDevApp.ms-teams-vscode-extension
